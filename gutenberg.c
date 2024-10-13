6b84598a8512ce3954d6d6442d51b2890a682892
Maartz <maartz@protonmail.com>
2024-10-13 19:52:56 +0200

    print char and ASCII code value

--- gutenberg.c
+++ gutenberg.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -30,6 +32,11 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    }
+    printf("%d ('%c')\n", c, c);
+  }
   return 0;
 }
