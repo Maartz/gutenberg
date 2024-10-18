5969f6da2750584333027e6b49c468218576425b
Maartz <maartz@protonmail.com>
2024-10-18 11:35:20 +0200

    optimize line cleaning

--- gutenberg.c
+++ gutenberg.c
@@ -150,6 +150,7 @@ void editorDrawRows(struct abuf *ab) {
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -163,8 +164,6 @@ void editorRefreshScreen(void) {
 
   // hide the cursor
   abAppend(&ab, "\x1b[?25l", 6);
-
-  abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
